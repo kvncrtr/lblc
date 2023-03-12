82a51dc35eeb87045b5af8da2568ee931640c475
Kevin Carter <console.logkevin@gmail.com>
2023-03-12 08:06:13 -0400

    add nav foundation and import logo asset

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,34 @@ import React from 'react'
 import './App.css'
 import Nav from './components/Nav'
 
+// assets 
+import logo from './assets/nav-logo.png'
+
 function App() {
   return (
     <div className="App">
-      
+      <header className='primary-header' >
+        {/* logo 
+        add a className and others to this node
+        */}
+        <img src={logo} alt='little brook learning center&#39s logo' />
+        
+        {/* navbar 
+         add class utilities to these and swap for links 
+         when we intergrate react router 
+         */} 
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Enrollment</li>
+            <li>For Parents</li>
+            <li>Gallery</li>
+            <li>Testimonies</li>
+          </ul>
+        </nav>
+
+      </header>
     </div>
   )
 }
